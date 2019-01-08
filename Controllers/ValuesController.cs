3aa491b250488d9949f3abe0b7eafb4644ef139e
thr828 <592040808@qq.com>
2019-01-08 23:54:28 +0800

    123

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,49 @@
+﻿using System; 
+using System.Collections.Generic; 
+using System.Linq; 
+using System.Threading.Tasks; 
+using Microsoft.AspNetCore.Mvc; 
+using Microsoft.AspNetCore.Cors; 
+
+///http://www.cnblogs.com/hbb0b0/p/8035241.html
+
+///public void OnActionExecuted(ActionExecutedContext actionExecutedContext)
+///{
+///var rs = actionExecutedContext.HttpContext.Response;
+///rs.Headers.Add("Access-Control-Allow-Origin", "*");
+///rs.Headers.Add("Access-Control-Allow-Headers", "*");
+///rs.Headers.Add("Access-Control-Allow-Methods", "*");
+///...
+///}
+namespace CoreWebApi.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController:ControllerBase {
+       // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult < string > Get(int id) {
+            return "value"; 
+        }
+        // GET api/values
+        [HttpGet]
+        [EnableCors("CorsSample")]
+        public IEnumerable < string > Get() {
+            return new string[] {DateTime.Now.ToString()}; 
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value) {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value) {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id) {
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Cors;
+
+namespace CoreWebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+              //添加cors 服务
+            services.AddCors(options =>options.AddPolicy("CorsSample",p => p.WithOrigins("http://localhost:5000")
+            .AllowAnyMethod().AllowAnyHeader()));
+           
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+            //配置Cors
+            app.UseCors("CorsSample");
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
